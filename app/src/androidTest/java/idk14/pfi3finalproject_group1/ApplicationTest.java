9969a279c69f6f3e588999b70a96ccbed582f341
lstenstadvold <lstenstadvold@gmail.com>
2015-05-05 10:19:54 +0200

    Initial commit

---
+++ app/src/androidTest/java/idk14/pfi3finalproject_group1/ApplicationTest.java
@@ -0,0 +1,13 @@
+package idk14.pfi3finalproject_group1;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

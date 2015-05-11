6ba191f576835012b04fb7e7310c17d2b66c64e5
lstenstadvold <lstenstadvold@gmail.com>
2015-05-11 14:28:35 +0200

    Added internet permission, Constants, Firbase stuff.

---
+++ app/src/main/java/idk14/pfi3finalproject_group1/Constants.java
@@ -0,0 +1,8 @@
+package idk14.pfi3finalproject_group1;
+
+/**
+ * Created by larsy09 on 5/11/15.
+ */
+public class Constants {
+    String firebaseURL = "https://blinding-torch-9917.firebaseio.com";
+}

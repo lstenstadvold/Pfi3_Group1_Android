d3c804f6709de7a4a1ba349019a83b66ec11b10c
Jesper Andersson <jsprA86@gmail.com>
2015-05-11 18:10:17 +0200

    Jag testar push

--- app/src/main/java/idk14/pfi3finalproject_group1/Constants.java
+++ app/src/main/java/idk14/pfi3finalproject_group1/Constants.java
@@ -4,5 +4,6 @@ package idk14.pfi3finalproject_group1;
  * Created by larsy09 on 5/11/15.
  */
 public class Constants {
+	//Detta är en test kommentar
     static final String FIREBASE_URL = "https://blinding-torch-9917.firebaseio.com";
 }
